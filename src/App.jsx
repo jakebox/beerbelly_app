a6401a6a97370b588f53a4c4d2c3cc854d6bb833
Jake B <jacobboxerman@gmail.com>
2024-10-10 00:41:35 -0400

    Make not bad for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Title, Text, TextInput } from "@mantine/core";
+import { Button, Center, Title, Text, TextInput } from "@mantine/core";
 import "@mantine/core/styles.css";
 import "./Gambarino/Gambarino.css";
 import "./index.css";
@@ -41,7 +41,7 @@ function App() {
   return (
     <div className="bg-gradient-to-r from-teal-950 via-green-950 to-emerald-900 h-screen">
       <div>
-        <div className="h-screen flex items-center justify-center px-16 flex-col">
+        <div className="h-screen flex items-center justify-center px-10 flex-col">
           <div className="flex flex-row items-center mb-3">
             <Title c="white" className="text-5xl mr-5">
               Beer Belly
@@ -50,28 +50,33 @@ function App() {
               A new way to rate, share, and enjoy your beers.
             </Text>
           </div>
-          <div className="flex flex-col items-center">
-            <Text c="white" mb={22} my={20}>
-              Sign up to be notified when our public beta opens, and become one of the first users of Beer Belly.
-            </Text>
-            <form onSubmit={handleSubmit}>
-              <TextInput
-                className="text-white"
-                placeholder="hello@gmail.com"
-                label="Email address"
-                size="md"
-                withAsterisk
-                value={email}
-                mb={4}
-                onChange={(event) => setEmail(event.currentTarget.value)}
-                required
-              />
+          <Text c="white" mb={22} my={20}>
+            Sign up to be notified when our public beta opens, and become one of the first users of Beer Belly.
+          </Text>
+          <form onSubmit={handleSubmit}>
+            <TextInput
+              className="text-white"
+              placeholder="hello@gmail.com"
+              label="Email address"
+              size="md"
+              withAsterisk
+              value={email}
+              mb={4}
+              w={250}
+              onChange={(event) => setEmail(event.currentTarget.value)}
+              required
+            />
+            <Center>
               <Button type="submit" my={10} variant="filled" disabled={isSubmitting}>
                 {isSubmitting ? "Submitting..." : "Submit"}
               </Button>
-            </form>
-            {message && <Text c="white" my={10}>{message}</Text>}
-          </div>
+            </Center>
+          </form>
+          {message && (
+            <Text c="white" my={10}>
+              {message}
+            </Text>
+          )}
           <Text c="white" my={30} size={"sm"} fs={"italic"}>
             © 2024 Null Pointer Industries
           </Text>
