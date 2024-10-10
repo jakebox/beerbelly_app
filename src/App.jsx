397460c3397bf569ba8d7fe62794865e41d5471e
Jake B <jacobboxerman@gmail.com>
2024-10-10 00:19:38 -0400

    Website

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Button, Space, Title, Text, TextInput } from "@mantine/core";
+import "@mantine/core/styles.css";
+import "./Gambarino/Gambarino.css";
+import "./index.css";
 
-import { MantineProvider } from '@mantine/core';
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [email, setEmail] = useState("");
 
   return (
-  <MantineProvider>
+    <div className="bg-gradient-to-r from-teal-950 via-green-950 to-emerald-900 h-screen">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="h-screen flex items-center justify-center px-16 flex-col">
+          <div className="flex flex-row items-center mb-3">
+            <Title c="white" className="text-5xl mr-5">
+              Beer Belly
+            </Title>
+            <Text c="white" fs={"italic"} className="text-lg">
+              A new way to rate, share, and enjoy your beers.
+            </Text>
+          </div>
+          <div className="flex flex-col items-center">
+            <Text c="white" mb={22} my={20}>
+              Sign up to be notified when our public beta opens, and become one of the first users of Beer Belly.
+            </Text>
+            <TextInput
+              className="text-white"
+              placeholder="hello@gmail.com"
+              label="Email address"
+              size="md"
+              withAsterisk
+              value={email}
+              mb={4}
+              onChange={(event) => setEmail(event.currentTarget.value)}
+            />
+            <Button my={10} variant="filled">
+              Submit
+            </Button>
+          </div>
+          <Text c="white" my={30} size={"sm"} fs={"italic"}>
+            © 2024 Null Pointer Industries
+          </Text>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </MantineProvider>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+
+import { MantineProvider, createTheme } from "@mantine/core";
+import "@mantine/core/styles.css";
+import "./Gambarino/Gambarino.css";
+
+const theme = createTheme({
+  fontFamily: "Switzer, sans-serif",
+  fontFamilyMonospace: "Monaco, Courier, monospace",
+  headings: { fontFamily: "Gambarino, sans-serif" },
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
+  </StrictMode>
+);
